c66c822e0dd753f23fc966c10b699e5de73ffe5c
Nathaniel Guieb <guiebn@CFPB-MBP15-Guieb.local>
2015-04-20 14:58:49 -0400

    removed unnecessary code

--- test/functional/cucumber/step_definitions/wizardNavigation.js
+++ test/functional/cucumber/step_definitions/wizardNavigation.js
@@ -4,30 +4,9 @@ chai.use(chaiAsPromised);
 
 var expect = chai.expect;
 
-var path = require('path');
-
 var navWizard = element(by.css(".nav-wizard"));
 
 module.exports = function() {
-
-    selectFile = function(fileName){
-        var deferred = protractor.promise.defer();
-        var fileSelector = element(by.id('file'));
-
-        //Get filename as argument, convert into path, send path to selector on site.
-        var fileToUpload = '../files/'+fileName;
-        var absolutePath = path.resolve(__dirname, fileToUpload);
-
-        fileSelector.sendKeys(absolutePath);
-        deferred.fulfill();
-        return deferred.promise;
-    }
-
-    this.When(/^I upload the "([^"]*)" file for validation$/, function (fileName, next) {
-        selectFile(fileName).then(function(){
-            next();
-        });
-    });
 
     this.Then(/^I see a navigation wizard$/, function (next) {
         expect(navWizard.isPresent()).to.eventually.be.true.notify(next);
